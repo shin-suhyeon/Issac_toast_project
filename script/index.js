f1a718b1fcb6741172df2e844d85fb10c11652e3
shin-suhyeon <165615546+shin-suhyeon@users.noreply.github.com>
2024-06-13 09:49:42 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,1 +1,15 @@
-const swiper = new Swiper('.swiper')
+const visual = new Swiper('.visual',{
+    autoplay:{delay:3000},
+    /* effect:'fade', */
+    effect:'cube',
+})
+const toast = new Swiper('.toast',{
+    autoplay:{delay:3000},
+    direction:'vertial'
+})
+const set = new Swiper('.set')
+const side = new Swiper('.side')
+const drink = new Swiper('.drink')
+const all = new Swiper('.all')
+const alram = new Swiper('.alram')
+const event = new Swiper('.event')
